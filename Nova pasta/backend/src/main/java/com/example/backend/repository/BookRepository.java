f9e82583ac6ea5feb143974ec9cb0080f180c631
Alexsandro S. de O. Júnior <alexsandro.s.junior6@ba.estudante.senai.br>
2025-10-02 00:27:05 +0000

    :tada:

--- Nova pasta/backend/src/main/java/com/example/backend/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Book;
-// import com.example.backend.service.BookService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {}
--- Nova pasta/biblioteca-frontend/src/services/api.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  // A URL base do seu backend.
-  // Se o seu backend estiver a correr noutra porta, altere aqui.
-  baseURL: 'http://localhost:8080'
-});
-
-export default api;
